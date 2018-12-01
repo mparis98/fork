fa5ce7fd7d944536e9be339e1d690e2f0db6c6e1
Thomas Dutrion <thomas@engineor.com>
2018-12-01 11:03:51 +0100

    Initial code

---
+++ index.php
@@ -0,0 +1,11 @@
+#!/usr/bin/php
+<?php
+
+use Support\Application;
+
+if (!is_file(__DIR__.'/vendor/autoload.php')) {
+    throw new LogicException('The autoload file does not exist, please use composer install');
+}
+require __DIR__.'/vendor/autoload.php';
+
+echo (new Application($argv, require __DIR__.'/config/di.global.php'))->run();
